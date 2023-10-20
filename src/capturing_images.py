e0d86962d9b74b1c4fd33c964886cc33a9d78067
Nigoo <nagarr@icloud.com>
2023-10-20 15:50:25 +0200

    first commit

---
+++ src/capturing_images.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+
+save_directory = "captured_images/person_name"  # change to the name of the person
+os.makedirs(save_directory, exist_ok=True)
+
+cap = cv2.VideoCapture(0)
+
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to grab frame")
+            break
+
+        cv2.imshow("Press 'c' to capture, 'q' to quit", frame)
+
+        key = cv2.waitKey(1)
+
+        if key == ord('c'):  # Press 'c' to capture the image
+            person_name = input("Enter the name of the person: ")
+            # Generate a unique filename
+            filename = f"{person_name}_{str(hash(frame.tobytes()))}.jpg"
+            file_path = os.path.join(save_directory, filename)
+            cv2.imwrite(file_path, frame)
+            print(f"Image saved to {file_path}")
+
+        elif key == ord('q'):  # Press 'q' to quit
+            break
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ src/encodings_creator.py
@@ -0,0 +1,51 @@
+import face_recognition
+import os
+import pickle
+
+face_images_directory = 'captured_images'  # Replace with your actual path
+
+known_faces = {}
+
+if not os.path.exists(face_images_directory):
+    print(f"The specified directory {face_images_directory} does not exist.")
+    exit()
+
+for foldername in os.listdir(face_images_directory):
+    folder_path = os.path.join(face_images_directory, foldername)
+
+    if os.path.isdir(folder_path):
+        name = foldername
+        print(f"Processing folder {name}")
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".jpg") or filename.endswith(".png"):
+                file_path = os.path.join(folder_path, filename)
+                print(f"Processing file {file_path}")
+
+                try:
+                    image = face_recognition.load_image_file(file_path)
+                except Exception as e:
+                    print(f"Failed to load image from {file_path} due to {str(e)}")
+                    continue
+
+                # Check for face locations before getting encodings
+                face_locations = face_recognition.face_locations(image)
+                print(f"Found {len(face_locations)} face(s) in {file_path}")
+
+                if face_locations:
+                    encodings = face_recognition.face_encodings(image, known_face_locations=face_locations)
+                    if encodings:
+                        if name in known_faces:
+                            known_faces[name].append(encodings[0])
+                        else:
+                            known_faces[name] = [encodings[0]]
+
+print("Encodings have been generated.")
+print(known_faces)
+
+filename = 'known_faces.pkl'
+
+with open(filename, 'wb') as file:
+    pickle.dump(known_faces, file)
+
+print(f"Encodings have been saved to {filename}")
